285821d39b7de2abb6cc3406e9a9eb3ee5088650
sugarylipss <iyeravisha@gmail.com>
2026-09-11 12:58:12 +0530

    Add envelope opening animation with letter display

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const envelope =
+    document.querySelector("#envelope");
 
+const openButton =
+    document.querySelector("#openButton");
+
+const letter =
+    document.querySelector("#letter");
+
+
+openButton.addEventListener(
+    "click",
+    function () {
+
+        envelope.classList.add("open");
+
+
+        setTimeout(
+            function () {
+
+                letter.classList.add("show");
+
+            },
+            700
+        );
+
+    }
+);
